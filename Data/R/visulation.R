2c644839fe62e84177def3a8bdcdd4c3a8fdd060
kris-geyer <33397635+kris-geyer@users.noreply.github.com>
2020-04-04 10:30:51 +0100

    Update visulation.R

--- Data/R/visulation.R
+++ Data/R/visulation.R
@@ -1,3 +1,5 @@
+
+ 
 library("plyr")
 library("dplyr")
 library("magrittr")
@@ -7,14 +9,15 @@ library("ggthemes")
 #past is the past.csv one from github -- run the cleaning script... and check variable names match
 setwd() #set to where past.csv is
 past = read_csv(past.csv) #read in csv
-
+ 
 #here we are taking the past df and looking at the average duration of usage by app
 past_mean = past %>%
   group_by(App) %>%
   summarise(mean_duration = mean(Duration)) 
-
+ 
 plotdata = subset(past_mean, App == "TikTok" | App == "YouTube" |
                     App == "WhatsApp" | App == "Gmail" | App == "Messages" | App == "WeChat")
+
 #then taking this and making a simple bar plot
 plotdata %>%
   ggplot(aes(reorder(App, -mean_duration), mean_duration)) +
@@ -22,3 +25,4 @@ plotdata %>%
   xlab("App") +
   ylab("Average Duration of Usage (seconds)") +
   theme_classic()
+ 
