63c37412e113f071578160b6fb92a147aeb751f2
Tina Keil <tina.keil@a4r.eu>
2022-07-17 20:28:04 +0200

    new simplified password generation

---
+++ app/src/main/java/geyerk/sensorlab/suselogger/GeneratePassword.java
@@ -0,0 +1,21 @@
+package geyerk.sensorlab.suselogger;
+
+import java.util.Random;
+
+/**
+ * Generate a random password
+ * added tk 17.06.2022
+ */
+
+public class GeneratePassword {
+
+    public static final String DATA = "123456789ABCDEFGHJKMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz!£$%&@#";
+    public static Random RANDOM = new Random();
+    public static String randomString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(DATA.charAt(RANDOM.nextInt(DATA.length())));
+        }
+        return sb.toString();
+    }
+}
