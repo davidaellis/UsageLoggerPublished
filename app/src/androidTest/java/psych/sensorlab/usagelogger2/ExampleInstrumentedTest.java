6b3d6a0becf8c545f1cf97a43fb197b0c0a95b03
Tina Keil <tina.keil@a4r.eu>
2022-08-12 00:30:35 +0200

    fixed depreciated methods

--- app/src/androidTest/java/psych/sensorlab/usagelogger2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/psych/sensorlab/usagelogger2/ExampleInstrumentedTest.java
@@ -2,26 +2,24 @@ package psych.sensorlab.usagelogger2;
 
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 /**
  * Instrumented test, which will execute on an Android device.
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("psych.sensorlab.usageloger2", appContext.getPackageName());
     }
 }
