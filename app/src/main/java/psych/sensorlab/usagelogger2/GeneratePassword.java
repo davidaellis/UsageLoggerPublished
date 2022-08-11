448676082f746aa947ef38b1247f6e894b2aa915
Tina Keil <tina.keil@a4r.eu>
2022-08-11 18:36:56 +0200

    updated chars that can be used for password

--- app/src/main/java/psych/sensorlab/usagelogger2/GeneratePassword.java
+++ app/src/main/java/psych/sensorlab/usagelogger2/GeneratePassword.java
@@ -6,10 +6,9 @@ import java.util.Random;
  * Generate a random password
  * added tk 17.06.2022
  */
-
 public class GeneratePassword {
 
-    public static final String DATA = "123456789ABCDEFGHJKMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz!£$%&@#";
+    public static final String DATA = "123456789ABCDEFGHKMNOPQRSTUVWXYZabcdefghkmnopqrstuvwxyz!$%&@#?";
     public static Random RANDOM = new Random();
     public static String randomString(int len) {
         StringBuilder sb = new StringBuilder(len);
